33a5ebbac54a31e5b6bcf2f68d5a654fb08d2ae2
mculinovic <marko.culinovic@gmail.com>
2015-04-12 18:49:03 +0200

    Test reading SAM files using seqan

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,39 @@
+// Copyright @mculinovic
 #include <iostream>
-#include <seqan/sequence.h>  // CharString, ...
-#include <seqan/stream.h>    // to stream a CharString into cout
+#include <seqan/bam_io.h>
 
-int main(int, char const **)
-{
-  std::cout << "Hello World!" << std::endl;
-  seqan::CharString mySeqAnString = "Hello SeqAn!";
-  std::cout << mySeqAnString << std::endl;
-  return 1;
+using seqan::BamFileIn;
+using seqan::BamHeader;
+using seqan::readHeader;
+using seqan::BamAlignmentRecord;
+using seqan::atEnd;
+using seqan::FormattedFileContext;
+
+using seqan::contigNames;
+using seqan::contigLengths;
+
+int main() {
+    BamFileIn bamFileIn("../data/E-Coli/e-coli.sam");
+
+    BamHeader header;
+    readHeader(header, bamFileIn);
+
+    typedef FormattedFileContext<BamFileIn, void>::Type TBamContext;
+    TBamContext const & bamContext = context(bamFileIn);
+
+    for (unsigned i = 0; i < length(contigNames(bamContext)); ++i)
+        std::cout << contigNames(bamContext)[i] << '\t'
+                  << contigLengths(bamContext)[i] << '\n';
+
+    BamAlignmentRecord record;
+    while (!atEnd(bamFileIn)) {
+        readRecord(record, bamFileIn);
+        std::cout << record.qName << std::endl;
+        for (auto& e: record.cigar) {
+            std::cout << e.count << e.operation << " ";
+        }
+        std::cout << std::endl;
+        std::cout << record.seq << std::endl;
+    }
+    return 0;
 }
