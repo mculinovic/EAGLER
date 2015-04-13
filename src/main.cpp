79c3041c19145c4b39e16076bc8cb81841c2db83
mculinovic <marko.culinovic@gmail.com>
2015-04-13 21:02:02 +0200

    Reading fasta files

--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,52 @@
 // Copyright @mculinovic
+#include <seqan/sequence.h>
+#include <parsero/parsero.h>
 #include <iostream>
-#include <seqan/bam_io.h>
+#include "./fasta_IO.h"
 
-using seqan::BamFileIn;
-using seqan::BamHeader;
-using seqan::readHeader;
-using seqan::BamAlignmentRecord;
-using seqan::atEnd;
-using seqan::FormattedFileContext;
 
-using seqan::contigNames;
-using seqan::contigLengths;
+using seqan::StringSet;
+using seqan::CharString;
+using seqan::Dna5String;
 
-int main() {
-    BamFileIn bamFileIn("../data/E-Coli/e-coli.sam");
 
-    BamHeader header;
-    readHeader(header, bamFileIn);
+char *tmp_contig_filename = "../data/contig_tmp.fasta";
+char *ont_reads_filename = nullptr;
+char *draft_genome_filename = nullptr;
 
-    typedef FormattedFileContext<BamFileIn, void>::Type TBamContext;
-    TBamContext const & bamContext = context(bamFileIn);
+// using parsero library for command line settings
+void setup_cmd_interface(int argc, char **argv) {
+    // argument - oxford nanopore reads in fasta format
+    parsero::add_argument("ont_reads.fasta",
+        [] (char *filename) { ont_reads_filename = filename; });
+    // argument - draft genome in fasta format
+    parsero::add_argument("draft_genome.fasta",
+        [] (char * filename) { draft_genome_filename = filename; });
 
-    for (unsigned i = 0; i < length(contigNames(bamContext)); ++i)
-        std::cout << contigNames(bamContext)[i] << '\t'
-                  << contigLengths(bamContext)[i] << '\n';
+    parsero::parse(argc, argv);
+}
 
-    BamAlignmentRecord record;
-    while (!atEnd(bamFileIn)) {
-        readRecord(record, bamFileIn);
-        std::cout << record.qName << std::endl;
-        for (auto& e: record.cigar) {
-            std::cout << e.count << e.operation << " ";
-        }
-        std::cout << std::endl;
-        std::cout << record.seq << std::endl;
+
+int main(int argc, char **argv) {
+    setup_cmd_interface(argc, argv);
+
+    if (ont_reads_filename == nullptr || draft_genome_filename == nullptr) {
+        parsero::help(argv[0]);
+        exit(1);
     }
+
+    StringSet<CharString> read_ids;
+    StringSet<Dna5String> read_seqs;
+    read_fasta(&read_ids, &read_seqs, ont_reads_filename);
+
+    StringSet<CharString> contig_ids;
+    StringSet<Dna5String> contig_seqs;
+    read_fasta(&contig_ids, &contig_seqs, draft_genome_filename);
+
+
+    for (int i = 0; i < length(read_ids); ++i) {
+        std::cout << read_ids[i] << '\t' << read_seqs[i] << '\n';
+    }
+
     return 0;
 }
