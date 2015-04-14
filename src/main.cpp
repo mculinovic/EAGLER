7814cb31e6aeb3ec357a920ae3ed373353f21fe3
mculinovic <marko.culinovic@gmail.com>
2015-04-14 13:12:01 +0200

    Main calls alignment for every contig

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,8 @@
 #include <seqan/sequence.h>
 #include <parsero/parsero.h>
 #include <iostream>
-#include "./fasta_IO.h"
+#include "./utility.h"
+#include "./bwa.h"
 
 
 using seqan::StringSet;
@@ -10,7 +11,6 @@ using seqan::CharString;
 using seqan::Dna5String;
 
 
-char *tmp_contig_filename = "../data/contig_tmp.fasta";
 char *ont_reads_filename = nullptr;
 char *draft_genome_filename = nullptr;
 
@@ -35,17 +35,25 @@ int main(int argc, char **argv) {
         exit(1);
     }
 
+    // read oxford nanopore long reads from file
     StringSet<CharString> read_ids;
     StringSet<Dna5String> read_seqs;
-    read_fasta(&read_ids, &read_seqs, ont_reads_filename);
+    utility::read_fasta(&read_ids, &read_seqs, ont_reads_filename);
 
+    // read contigs from draft genome file
     StringSet<CharString> contig_ids;
     StringSet<Dna5String> contig_seqs;
-    read_fasta(&contig_ids, &contig_seqs, draft_genome_filename);
+    utility::read_fasta(&contig_ids, &contig_seqs, draft_genome_filename);
 
+    int contigs_size = length(contig_ids);
+    for (int i = 0; i < contigs_size; ++i) {
+        // for every contig do following
 
-    for (int i = 0; i < length(read_ids); ++i) {
-        std::cout << read_ids[i] << '\t' << read_seqs[i] << '\n';
+        // 1. align reads to it using bwa
+        aligner::align(contig_ids[i], contig_seqs[i], ont_reads_filename);
+
+        // 2. try to extend it using alignments
+        // TODO(mculinovic, lukasterbic): extension algorithm
     }
 
     return 0;
