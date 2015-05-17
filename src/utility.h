570c3739f7aeb526e9b7740aac85ad5c5b2c5c13
Luka Sterbic <luka.sterbic@gmail.com>
2015-05-17 13:16:56 +0200

    moved implementation of template function from cpp to h

--- src/utility.h
+++ src/utility.h
@@ -184,7 +184,16 @@ char idx_to_base(int idx);
  * @tparam T the type of exception to be thrown
  */
 template<typename T>
-void throw_exception(const char *format, ...);
+void throw_exception(const char *format, ...) {
+    va_list args_list;
+    va_start(args_list, format);
+
+    vsnprintf(error_buffer, ERROR_BUFFER_SIZE, format, args_list);
+    va_end(args_list);
+
+    throw T(error_buffer);
+}
+
 
 /**
  * @brief Prints error message and exits
