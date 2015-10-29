bc96e212059632152834f07f1e0da0b19879914f
Luka Sterbic <luka.sterbic@gmail.com>
2015-10-29 20:31:02 +0100

    GraphMap wrapper

---
+++ src/graphmap.cpp
@@ -0,0 +1,57 @@
+#include "graphmap.h"
+#include "utility.h"
+
+void GraphmapAligner::index(const char* filename) {
+	utility::execute_command("graphmap -v 0 -I -r %s", filename);
+}
+
+void GraphmapAligner::align(const char* reference_file,
+                            const char* reads_file) {
+    utility::execute_command(
+        "graphmap -v 0 -t %d -r %s -d %d -o %s",
+        utility::get_concurrency_level(),
+        reference_file,
+        reads_file,
+        get_tmp_alignment_filename()
+    );
+}
+
+void GraphmapAligner::align(const char* reference_file,
+                            const char* reads_file,
+                            const char* sam_file,
+                            bool only_primary) {
+    // TODO handle only primary
+    utility::execute_command(
+        "graphmap -v 0 -t %d -r %s -d %d -o %s",
+        utility::get_concurrency_level(),
+        reference_file,
+        reads_file,
+        sam_file
+    );
+}
+
+void GraphmapAligner::align(const char* reference_file,
+                            const char* reads_file,
+                            const char* sam_file) {
+    // TODO handle only primary
+    utility::execute_command(
+        "graphmap -v 0 -t %d -r %s -d %d -o %s",
+        utility::get_concurrency_level(),
+        reference_file,
+        reads_file,
+        sam_file
+    );
+}
+
+void GraphmapAligner::align(const CharString& id,
+                            const Dna5String& contig,
+                            const char* reads_filename) {
+    // write contig to temporary .fasta file
+    utility::write_fasta(id, contig, get_tmp_contig_filename());
+
+    // create index for contig
+    index(get_tmp_contig_filename());
+
+    // align reads to conting
+    align(get_tmp_contig_filename(), reads_filename);
+}
---
+++ src/graphmap.h
@@ -0,0 +1,30 @@
+#ifndef GRAPHMAP_H
+#define GRAPHMAP_H
+
+#include <seqan/sequence.h>
+
+#include "aligner.h"
+
+using seqan::CharString;
+using seqan::Dna5String;
+
+
+class GraphmapAligner: public Aligner {
+public:
+	virtual ~GraphmapAligner() = default;
+	virtual void index(const char* filename);
+	virtual void align(const char* reference_file,
+	                   const char* reads_file);
+	virtual void align(const char* reference_file,
+	                   const char* reads_file,
+	                   const char* sam_file,
+	                   bool only_primary);
+	virtual void align(const char* reference_file,
+	                   const char* reads_file,
+	                   const char* sam_file);
+	virtual void align(const CharString& id,
+	                   const Dna5String& contig,
+	                   const char* reads_filename);
+};
+
+#endif // GRAPHMAP_H
