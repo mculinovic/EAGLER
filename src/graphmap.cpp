83ff62a73beaf281e4aec8f25549c989093f017e
mculinovic <marko.culinovic@gmail.com>
2015-11-03 21:49:13 +0100

    graphmap: fix parameters order and function wrapping

--- src/graphmap.cpp
+++ src/graphmap.cpp
@@ -7,13 +7,14 @@ void GraphMapAligner::index(const char* filename) {
 
 void GraphMapAligner::align(const char* reference_file,
                             const char* reads_file) {
-    utility::execute_command(
-        "graphmap -v 0 -t %d -r %s -d %s -o %s",
-        utility::get_concurrency_level(),
-        reference_file,
-        reads_file,
-        get_tmp_alignment_filename()
-    );
+    // utility::execute_command(
+    //    "graphmap -v 0 -t %d -r %s -d %s -o %s",
+    //    utility::get_concurrency_level(),
+    //    reference_file,
+    //    reads_file,
+    //    get_tmp_alignment_filename()
+    //);
+    align(reference_file, reads_file, get_tmp_alignment_filename(), false);
 }
 
 void GraphMapAligner::align(const char* reference_file,
@@ -23,8 +24,8 @@ void GraphMapAligner::align(const char* reference_file,
     // TODO handle only primary
     utility::execute_command(
         "graphmap -v 0 -t %d %s -r %s -d %s -o %s",
-        only_primary ? "" : "-Z",
         utility::get_concurrency_level(),
+	only_primary ? "" : "-Z",
         reference_file,
         reads_file,
         sam_file
@@ -35,13 +36,14 @@ void GraphMapAligner::align(const char* reference_file,
                             const char* reads_file,
                             const char* sam_file) {
     // TODO handle only primary
-    utility::execute_command(
-        "graphmap -v 0 -t %d -r %s -d %s -o %s",
-        utility::get_concurrency_level(),
-        reference_file,
-        reads_file,
-        sam_file
-    );
+    //utility::execute_command(
+    //    "graphmap -v 0 -t %d -r %s -d %s -o %s",
+    //    utility::get_concurrency_level(),
+    //    reference_file,
+    //    reads_file,
+    //    sam_file
+    //);
+    align(reference_file, reads_file, sam_file, false);
 }
 
 void GraphMapAligner::align(const CharString& id,
